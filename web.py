c82fccefb8dd64f2c0c2d7f17e43eb49f92a417e
Clint Grimsley <clint.grimsley@gmail.com>
2021-08-19 01:15:33 -0400

    added bootstrap css because this business was ugly as sin

--- web.py
+++ web.py
@@ -50,37 +50,45 @@ urlbase = 'https://airtable.com/shrL8Ozj2HE2G8LEO?prefill_Which+dog+are+you+appl
 @app.route('/')
 def index():
     html = """
-    <div style="width: 500px">
-        <h1>Hope For Pits URL Maker</h1>
-        <h2>new method (ultra janky)</h2>
-        <p>Just load the page and the textarea is already prepopulated with the json. 
-            what makes this work is incredibly janky and I imagine that it probably needs
-            to be refreshed every day, but it's working as of now. Yes, I probably should 
-            just make it all happen in the background, but for some reason adding a new api
-            endpoint and calling it from javascript seemed like the thing to do.</p>
-        <p>if you think this sucks, keep it to yourself, or bother me, idc.</p>
-        <h2>old method</h2>
-        <p>To use the url maker you'll need to use chrome developer tool, if you're running 
-            chrome you can open with F12. Here's 
-            <a href="https://developer.chrome.com/docs/devtools/open/">a tutorial</a> on chrome 
-            dev tools if you're curious.</p>
-        <p>Use chrome dev tools on <a href="https://airtable.com/shrL8Ozj2HE2G8LEO" target="_blank">
-            adoption application page</a> to retrieve the response from GetRowsMatchingName GET request
-            and paste it here. It will look something like this:</p>
-        <pre>{"msg":"SUCCESS","data":{"rowResults":[{"id":"recAQBiyRQVOpUUai","createdTime":"2021-07-07T18:53:31.000Z","cellValuesByColumnId":{"fldk63tdzoGOMN8JR":"Brandon"}}...</pre>
-        <form action="/make_urls" method="post">
-            <label>Pet list JSON</label>
-            <br />
-            <textarea id="petlisttextarea" name="petlist" style="width:600px; height:300px;"></textarea>
-            <p><input type="submit" value="submit"></p>
-        </form>
-        <script type="text/javascript">
-        fetch("/get/curl",{
-            method: "GET"
-        }).then(response => response.json())
-        .then(data => document.getElementById("petlisttextarea").value = JSON.stringify(data))
-        </script>
-    </div>
+    <html>
+        <head>            
+            <link rel="stylesheet" href="https://unpkg.com/bootstrap@4.1.1/dist/css/bootstrap.css">
+        </head>
+        <body>          
+            <div class="container">  
+                <h1>Hope For Pits URL Maker</h1>
+                <h2>new method (ultra janky)</h2>
+                <p>Just load the page and the textarea is already prepopulated with the json. 
+                    what makes this work is incredibly janky and I imagine that it probably needs
+                    to be refreshed every day, but it's working as of now. Yes, I probably should 
+                    just make it all happen in the background, but for some reason adding a new api
+                    endpoint and calling it from javascript seemed like the thing to do.</p>
+                <p>if you think this sucks, keep it to yourself, or bother me, idc.</p>
+                <h2>old method</h2>
+                <p>To use the url maker you'll need to use chrome developer tool, if you're running 
+                    chrome you can open with F12. Here's 
+                    <a href="https://developer.chrome.com/docs/devtools/open/">a tutorial</a> on chrome 
+                    dev tools if you're curious.</p>
+                <p>Use chrome dev tools on <a href="https://airtable.com/shrL8Ozj2HE2G8LEO" target="_blank">
+                    adoption application page</a> to retrieve the response from GetRowsMatchingName GET request
+                    and paste it here. It will look something like this:</p>
+                <pre>{"msg":"SUCCESS","data":{"rowResults":[{"id":"recAQBiyRQVOpUUai","createdTime":"2021-07-07T18:53:31.000Z","cellValuesByColumnId":{"fldk63tdzoGOMN8JR":"Brandon"}}...</pre>
+                <form action="/make_urls" method="post">
+                    <label>Pet list JSON</label>
+                    <br />
+                    <textarea id="petlisttextarea" name="petlist" style="width:600px; height:300px;"></textarea>
+                    <p><input type="submit" value="submit"></p>
+                </form>
+                <script type="text/javascript">
+                fetch("/get/curl",{
+                    method: "GET"
+                }).then(response => response.json())
+                .then(data => document.getElementById("petlisttextarea").value = JSON.stringify(data))
+                </script>            
+            </div>
+        </body>
+    </html>
+    
     """
     return html
 
